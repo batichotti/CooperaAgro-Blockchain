2d6355d7f09545663ef8f6c23ef47183454dcf52
Defendi <tiagodefendidasilva@gmail.com>
2026-03-03 14:19:07 -0300

    estrutura do projeto

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,38 +1,29 @@
-import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
-import { configVariable, defineConfig } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-ethers";
+import * as dotenv from "dotenv";
 
-export default defineConfig({
-  plugins: [hardhatToolboxMochaEthersPlugin],
-  solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
-      },
-      production: {
-        version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-    },
-  },
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+
   networks: {
-    hardhatMainnet: {
-      type: "edr-simulated",
-      chainType: "l1",
-    },
-    hardhatOp: {
-      type: "edr-simulated",
-      chainType: "op",
-    },
     sepolia: {
       type: "http",
-      chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      url: process.env.SEPOLIA_URL!,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      chainId: 11155111,
+    },
+
+    amoy: {
+      type: "http",
+      url: process.env.AMOY_URL!,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      chainId: 80002,
     },
   },
-});
+};
+
+export default config;
