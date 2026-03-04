4950286797cc3d58107218ee8b93d246e1310bc3
Matt Batichotti <matebatichotti@gmail.com>
2026-03-04 10:13:37 -0300

    Enhancing CooperaAgro tracking contract

--- src/privado/CooperaAgro.sol
+++ src/privado/CooperaAgro.sol
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.31;
-
-contract CooperaAgro {
+// L1: Deploy custou 0,002354 ETH + 1 Ciclo completo custou 0,001745 ETH
+contract CooperaAgro { 
 
     enum ESTADO_DO_CONTRATO {
-        INEXISTENTE, // 0
-        CONTRATO_CRIADO, // 1
-        PRODUTOS_OFERTADOS, // 2
-        PRODUTOS_COMPRADOS, // 3
-        PACOTE_ENVIADO_PARA_COOPERATIVA, // 4
-        PACOTE_RECEBIDO_PELA_COOPERATIVA, // 5
-        ENVIANDO_PACOTES_PARA_ESCOLAS, // 6
-        TODOS_PACOTES_RECEBIDO_PELAS_ESCOLAS // 7
+        CONTRATO_CRIADO, // 0
+        PRODUTOS_OFERTADOS, // 1
+        PRODUTOS_COMPRADOS, // 2
+        PACOTE_ENVIADO_PARA_COOPERATIVA, // 3
+        PACOTE_RECEBIDO_PELA_COOPERATIVA, // 4
+        ENVIANDO_PACOTES_PARA_ESCOLAS, // 5
+        TODOS_PACOTES_RECEBIDO_PELAS_ESCOLAS // 6
     }
 
     struct Item {
@@ -31,11 +30,8 @@ contract CooperaAgro {
     mapping(uint256 => uint256[]) public valores_por_contrato;
 
     // Relacionamento: id_contrato => id_escola => Itens
-    mapping(uint256 => mapping(uint256 => Item[])) private pacotes_escolas;
-    // Relacionamento: id_contrato => id_escola => se recebeu
-    mapping(uint256 => mapping(uint256 => bool)) public escola_confirmou;
-    
-    mapping(uint256 => uint256) public contagem_entregas_escolas;
+    mapping(uint256 => mapping(uint256 => Item[])) private pacotes_escolas;    
+    mapping(uint256 => uint256[]) public escolas_que_confirmaram;
 
     // --- Modifiers ---
     modifier apenasNoEstado(uint256 _id_contrato, ESTADO_DO_CONTRATO _estado) {
@@ -45,19 +41,12 @@ contract CooperaAgro {
 
     // --- Funções Principais ---
 
-    function criarContrato(uint256 _id_contrato) public {
-        require(estados[_id_contrato] == ESTADO_DO_CONTRATO.INEXISTENTE, "ID ja existe");
-        estados[_id_contrato] = ESTADO_DO_CONTRATO.CONTRATO_CRIADO;
-    }
-
     function produtorOfertar(uint256 _id_contrato, uint256 _id_produtor, Item[] memory _oferta) 
         public 
         apenasNoEstado(_id_contrato, ESTADO_DO_CONTRATO.CONTRATO_CRIADO) 
     {
         produtores[_id_contrato] = _id_produtor;
-        for (uint i = 0; i < _oferta.length; i++) {
-            ofertas[_id_contrato].push(_oferta[i]);
-        }
+        ofertas[_id_contrato] = _oferta;
         estados[_id_contrato] = ESTADO_DO_CONTRATO.PRODUTOS_OFERTADOS;
     }
 
@@ -67,10 +56,7 @@ contract CooperaAgro {
     {
         cooperativas[_id_contrato] = _id_cooperativa;
         valores_por_contrato[_id_contrato] = _valores;
-        
-        for (uint i = 0; i < _compra.length; i++) {
-            compras[_id_contrato].push(_compra[i]);
-        }
+        compras[_id_contrato] = _compra;
         estados[_id_contrato] = ESTADO_DO_CONTRATO.PRODUTOS_COMPRADOS;
     }
 
@@ -88,7 +74,7 @@ contract CooperaAgro {
         estados[_id_contrato] = ESTADO_DO_CONTRATO.PACOTE_RECEBIDO_PELA_COOPERATIVA;
     }
 
-    function cooperativaEnviarPacote(uint256 _id_contrato, uint256 _id_escola, Item[] memory _pacote) 
+    function cooperativaEnviarPacote(uint256 _id_contrato, uint256 _id_escola, Item[] memory _pacote)
         public 
     {
         require(
@@ -98,47 +84,22 @@ contract CooperaAgro {
         );
 
         // Registro da escola no contrato
-        bool existe = false;
-        uint256[] storage esc = escolas_por_contrato[_id_contrato];
-        for(uint i=0; i < esc.length; i++) {
-            if(esc[i] == _id_escola) { existe = true; break; }
-        }
-        if(!existe) esc.push(_id_escola);
+        escolas_por_contrato[_id_contrato].push(_id_escola);
 
-        // Abate estoque do contrato específico
-        Item[] storage estoque = compras[_id_contrato];
-        for (uint i = 0; i < _pacote.length; i++) {
-            bool achou = false;
-            for (uint j = 0; j < estoque.length; j++) {
-                if (estoque[j].id_produto == _pacote[i].id_produto) {
-                    require(estoque[j].qtd >= _pacote[i].qtd, "Estoque insuficiente");
-                    estoque[j].qtd -= _pacote[i].qtd;
-                    achou = true;
-                    break;
-                }
-            }
-            require(achou, "Produto nao comprado");
-            pacotes_escolas[_id_contrato][_id_escola].push(_pacote[i]);
+        pacotes_escolas[_id_contrato][_id_escola] = _pacote;
+        if (estados[_id_contrato] == ESTADO_DO_CONTRATO.ENVIANDO_PACOTES_PARA_ESCOLAS) {
+            estados[_id_contrato] = ESTADO_DO_CONTRATO.ENVIANDO_PACOTES_PARA_ESCOLAS;
         }
-        estados[_id_contrato] = ESTADO_DO_CONTRATO.ENVIANDO_PACOTES_PARA_ESCOLAS;
     }
 
-    function escolaConfirmarEntrega(uint256 _id_contrato, uint256 _id_escola) 
+    function escolaConfirmarEntrega(uint256 _id_contrato, uint256 _id_escola, bool _tem_sobra_no_estoque) 
         public 
         apenasNoEstado(_id_contrato, ESTADO_DO_CONTRATO.ENVIANDO_PACOTES_PARA_ESCOLAS) 
     {
-        require(!escola_confirmou[_id_contrato][_id_escola], "Ja confirmado");
-        escola_confirmou[_id_contrato][_id_escola] = true;
-        contagem_entregas_escolas[_id_contrato]++;
+        escolas_que_confirmaram[_id_contrato].push(_id_escola);
 
         // Validação de estoque zerado para o contrato específico
-        uint256 saldoTotal = 0;
-        Item[] storage estoque = compras[_id_contrato];
-        for (uint i = 0; i < estoque.length; i++) {
-            saldoTotal += estoque[i].qtd;
-        }
-
-        if (contagem_entregas_escolas[_id_contrato] == escolas_por_contrato[_id_contrato].length && saldoTotal == 0) {
+        if ( (escolas_que_confirmaram[_id_contrato].length == escolas_por_contrato[_id_contrato].length) && !_tem_sobra_no_estoque) {
             estados[_id_contrato] = ESTADO_DO_CONTRATO.TODOS_PACOTES_RECEBIDO_PELAS_ESCOLAS;
         }
     }
