6330addfaebd3af00d73e660c563a798a3348c66
Defendi <tiagodefendidasilva@gmail.com>
2026-03-10 13:28:21 -0300

    custos

--- src/privado/CooperaAgro.sol
+++ src/privado/CooperaAgro.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.31;
-// L1: Deploy custou 0,0020 ETH + 1 Ciclo que custou 0,0021 ETH
+// L1 (RemixIDE): Deploy custou 0,0020 ETH + 1 Ciclo que custou 0,0021 ETH
+// L1 (Foundry): Deploy custou 0,000001 ETH + 1 Ciclo que custou 0,00XX ETH
 // L2: Deploy custou 0,00XX ETH + 1 Ciclo que custou 0,00XX ETH
-contract CooperaAgro { 
+contract CooperaAgro {
 
     enum ESTADO_DO_CONTRATO {
         CONTRATO_CRIADO, // 0
@@ -25,7 +26,7 @@ contract CooperaAgro {
     mapping(uint256 => uint256) public produtores;
     mapping(uint256 => uint256) public cooperativas;
     mapping(uint256 => uint256[]) public escolas_por_contrato;
-    
+
     mapping(uint256 => Item[]) private ofertas;
     mapping(uint256 => Item[]) private compras;
     mapping(uint256 => uint256[]) public valores_por_contrato;
@@ -33,7 +34,7 @@ contract CooperaAgro {
     mapping(uint256 => Item[]) private pacotes_recebidos_cooperativa;
 
     // Relacionamento: id_contrato => id_escola => Itens
-    mapping(uint256 => mapping(uint256 => Item[])) private pacotes_escolas;    
+    mapping(uint256 => mapping(uint256 => Item[])) private pacotes_escolas;
     mapping(uint256 => mapping(uint256 => Item[])) private pacotes_recebidos_por_escolas;
     mapping(uint256 => uint256) public escolas_que_confirmaram;
 
@@ -45,9 +46,9 @@ contract CooperaAgro {
 
     // --- Funções Principais ---
 
-    function produtorOfertar(uint256 _id_contrato, uint256 _id_produtor, Item[] memory _oferta) 
-        public 
-        apenasNoEstado(_id_contrato, ESTADO_DO_CONTRATO.CONTRATO_CRIADO) 
+    function produtorOfertar(uint256 _id_contrato, uint256 _id_produtor, Item[] memory _oferta)
+        public
+        apenasNoEstado(_id_contrato, ESTADO_DO_CONTRATO.CONTRATO_CRIADO)
     {
         produtores[_id_contrato] = _id_produtor;
         for (uint i = 0; i < _oferta.length; i++) {
@@ -56,9 +57,9 @@ contract CooperaAgro {
         estados[_id_contrato] = ESTADO_DO_CONTRATO.PRODUTOS_OFERTADOS;
     }
 
-    function cooperativaComprar(uint256 _id_contrato, uint256 _id_cooperativa, Item[] memory _compra, uint256[] memory _valores) 
-        public 
-        apenasNoEstado(_id_contrato, ESTADO_DO_CONTRATO.PRODUTOS_OFERTADOS) 
+    function cooperativaComprar(uint256 _id_contrato, uint256 _id_cooperativa, Item[] memory _compra, uint256[] memory _valores)
+        public
+        apenasNoEstado(_id_contrato, ESTADO_DO_CONTRATO.PRODUTOS_OFERTADOS)
     {
         cooperativas[_id_contrato] = _id_cooperativa;
         valores_por_contrato[_id_contrato] = _valores;
@@ -68,9 +69,9 @@ contract CooperaAgro {
         estados[_id_contrato] = ESTADO_DO_CONTRATO.PRODUTOS_COMPRADOS;
     }
 
-    function produtorEnviarPacote(uint256 _id_contrato, Item[] memory _pacote) 
-        public 
-        apenasNoEstado(_id_contrato, ESTADO_DO_CONTRATO.PRODUTOS_COMPRADOS) 
+    function produtorEnviarPacote(uint256 _id_contrato, Item[] memory _pacote)
+        public
+        apenasNoEstado(_id_contrato, ESTADO_DO_CONTRATO.PRODUTOS_COMPRADOS)
     {
         for (uint i = 0; i < _pacote.length; i++) {
             pacotes_enviados_produtor[_id_contrato].push(_pacote[i]);
@@ -78,9 +79,9 @@ contract CooperaAgro {
         estados[_id_contrato] = ESTADO_DO_CONTRATO.PACOTE_ENVIADO_PARA_COOPERATIVA;
     }
 
-    function cooperativaConfirmarEntrega(uint256 _id_contrato, Item[] memory _pacote) 
-        public 
-        apenasNoEstado(_id_contrato, ESTADO_DO_CONTRATO.PACOTE_ENVIADO_PARA_COOPERATIVA) 
+    function cooperativaConfirmarEntrega(uint256 _id_contrato, Item[] memory _pacote)
+        public
+        apenasNoEstado(_id_contrato, ESTADO_DO_CONTRATO.PACOTE_ENVIADO_PARA_COOPERATIVA)
     {
         for (uint i = 0; i < _pacote.length; i++) {
             pacotes_recebidos_cooperativa[_id_contrato].push(_pacote[i]);
@@ -89,10 +90,10 @@ contract CooperaAgro {
     }
 
     function cooperativaEnviarPacote(uint256 _id_contrato, uint256 _id_escola, Item[] memory _pacote)
-        public 
+        public
     {
         require(
-            estados[_id_contrato] == ESTADO_DO_CONTRATO.PACOTE_RECEBIDO_PELA_COOPERATIVA || 
+            estados[_id_contrato] == ESTADO_DO_CONTRATO.PACOTE_RECEBIDO_PELA_COOPERATIVA ||
             estados[_id_contrato] == ESTADO_DO_CONTRATO.ENVIANDO_PACOTES_PARA_ESCOLAS,
             "Estado invalido"
         );
@@ -109,9 +110,9 @@ contract CooperaAgro {
         }
     }
 
-    function escolaConfirmarEntrega(uint256 _id_contrato, uint256 _id_escola, Item[] memory _pacote, bool _tem_sobra_no_estoque) 
-        public 
-        apenasNoEstado(_id_contrato, ESTADO_DO_CONTRATO.ENVIANDO_PACOTES_PARA_ESCOLAS) 
+    function escolaConfirmarEntrega(uint256 _id_contrato, uint256 _id_escola, Item[] memory _pacote, bool _tem_sobra_no_estoque)
+        public
+        apenasNoEstado(_id_contrato, ESTADO_DO_CONTRATO.ENVIANDO_PACOTES_PARA_ESCOLAS)
     {
         for (uint i = 0; i < _pacote.length; i++) {
             pacotes_recebidos_por_escolas[_id_contrato][_id_escola].push(_pacote[i]);
